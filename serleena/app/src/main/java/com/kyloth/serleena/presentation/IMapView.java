8be42f7bff434008eba531b925d812292b95966e
Tobia Tesan <tobia.tesan@gmail.com>
2015-05-06 00:09:49 +0200

    PRSNT: Aggiungi IMapView
    Related to: SHANDROID-69 SHANDROID-132

---
+++ serleena/app/src/main/java/com/kyloth/serleena/presentation/IMapView.java
@@ -0,0 +1,85 @@
+///////////////////////////////////////////////////////////////////////////////
+// 
+// This file is part of Serleena.
+// 
+// The MIT License (MIT)
+//
+// Copyright (C) 2015 Antonio Cavestro, Gabriele Pozzan, Matteo Lisotto, 
+//   Nicola Mometto, Filippo Sestini, Tobia Tesan, Sebastiano Valle.    
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * Name: IMapView
+ * Package: com.hitchikers.serleena.presentation
+ * Author: Tobia Tesan <tobia.tesan@gmail.com>
+ * Date: 2015-05-01
+ * 
+ * History: 
+ * Version    Programmer    Date        Changes
+ * 1.0        Tobia Tesan   2015-05-01  Creazione del file
+ */
+
+package com.kyloth.serleena.presentation;
+
+import com.kyloth.serleena.common.GeoPoint;
+import com.kyloth.serleena.common.IQuadrant;
+import com.kyloth.serleena.common.UserPoint;
+
+/**
+ * E' l'interfaccia realizzata da tutte le viste che sono in grado di
+ * mostrare una mappa con la posizione dell'utente, tra cui quella della
+ * schermata Mappa.
+ *
+ * @author Tobia Tesan <tobia.tesan@gmail.com>
+ * @version 1.0
+ * @since 1.0
+ */
+public interface IMapView {
+	/**
+	 * Imposta la posizione dell'utente per la visualizzazione.
+	 *
+	 * @since 1.0
+	 */
+	void setUserLocation(GeoPoint point);
+
+	/**
+	 * Imposta il quadrante di mappa da visualizzare.
+	 *
+	 * @since 1.0
+	 */
+	void displayQuadrant(IQuadrant q);
+
+	/**
+	 * Mostra una serie di Punti Utente.
+	 *
+	 * @since 1.0
+	 */
+	void displayUP(Iterable<UserPoint> points);
+
+	/**
+	 * Lega un Presenter alla vista.
+	 *
+	 * @since 1.0
+	 */
+	void attachPresenter(IMapPresenter presenter);
+}
