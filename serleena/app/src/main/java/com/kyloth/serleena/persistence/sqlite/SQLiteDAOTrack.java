aa8ec1930240e78e249c59cf1ec71201a4c18f15
Filippo Sestini <sestini.filippo@gmail.com>
2015-05-06 11:38:00 +0200

    SQLITE: Aggiungi stub di SQLiteDAOTrack.
    Related to SHANDROID-4

---
+++ serleena/app/src/main/java/com/kyloth/serleena/persistence/sqlite/SQLiteDAOTrack.java
@@ -0,0 +1,98 @@
+///////////////////////////////////////////////////////////////////////////////
+// 
+// This file is part of Serleena.
+// 
+// The MIT License (MIT)
+//
+// Copyright (C) 2015 Antonio Cavestro, Gabriele Pozzan, Matteo Lisotto, 
+//   Nicola Mometto, Filippo Sestini, Tobia Tesan, Sebastiano Valle.    
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * Name: SQLiteDAOTrack.java
+ * Package: com.kyloth.serleena.persistence.sqlite
+ * Author: Filippo Sestini
+ * Date: 2015-05-06
+ *
+ * History:
+ * Version  Programmer          Date        Changes
+ * 1.0.0    Filippo Sestini     2015-05-06  Creazione file e scrittura di codice
+ *                                          e documentazione in Javadoc.
+ */
+
+package com.kyloth.serleena.persistence.sqlite;
+
+import com.kyloth.serleena.common.TelemetryEvent;
+import com.kyloth.serleena.persistence.ITelemetryStorage;
+import com.kyloth.serleena.persistence.ITrackStorage;
+
+/**
+ * Implementazione di persistence.ITrackStorage per la persistenza su
+ * database SQLite integrato in Android.
+ *
+ * @author Filippo Sestini <sestini.filippo@gmail.com>
+ * @version 1.0.0
+ * @since 2015-05-06
+ * @see com.kyloth.serleena.persistence.ITrackStorage
+ */
+public class SQLiteDAOTrack implements ITrackStorage {
+
+    private int id;
+    private ISerleenaSQLiteDataSource dataSource;
+
+    public SQLiteDAOTrack(int id, ISerleenaSQLiteDataSource dataSource) {
+        this.id = id;
+        this.dataSource = dataSource;
+    }
+
+    /**
+     * Implementazione di ITrackStorage.createTelemetry().
+     *
+     * @param events Eventi da cui creare il Tracciamento.
+     */
+    @Override
+    public void createTelemetry(Iterable<TelemetryEvent> events) {
+        /*dataSource.createTelemetry(events, this);*/
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Implementazione di ITrackStorage.getTelemetries().
+     *
+     * @return Tracciamenti associati al Percorso.
+     */
+    @Override
+    public Iterable<ITelemetryStorage> getTelemetries() {
+        return null;
+    }
+
+    /**
+     * Restituisce l'ID dell'oggetto nella tabella di appartenenza.
+     *
+     * @return ID dell'oggetto esperienza.
+     */
+    public int id() {
+        return id;
+    }
+
+}
