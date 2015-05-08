e542cfcca2a53480225bf5d1b72cb16e8fa0c41a
Tobia Tesan <tobia.tesan@gmail.com>
2015-05-08 23:08:18 +0200

    PERS: Aggiungi template SerleenaDatabaseTest
    Related to: SHANDROID-136

---
+++ serleena/app/src/test/java/com/kyloth/serleena/persistence/sqlite/SerleenaDatabaseTest.java
@@ -0,0 +1,75 @@
+///////////////////////////////////////////////////////////////////////////////
+// 
+// This file is part of Serleena.
+// 
+// The MIT License (MIT)
+//
+// Copyright (C) 2015 Antonio Cavestro, Gabriele Pozzan, Matteo Lisotto, 
+//   Nicola Mometto, Filippo Sestini, Tobia Tesan, Sebastiano Valle.    
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * Name: SerleenaDatabaseTest.java
+ * Package: com.kyloth.serleena.persistence.sqlite
+ * Author: Tobia Tesan <tobia.tesan@gmail.com>
+ * Date: 2015-05-05
+ *
+ * History:
+ * Version  Programmer          Date        Changes
+ * 1.0      Tobia Tesan        2015-05-05  Creazione file
+ */
+
+package com.kyloth.serleena.persistence.sqlite;
+
+import android.database.sqlite.SQLiteDatabase;
+import com.kyloth.serleena.BuildConfig;
+import java.net.URISyntaxException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RuntimeEnvironment;
+
+/**
+ * Contiene i test di unità per la classe SerleenaDatabaseTest
+ *
+ * @author Tobia Tesan <tobia.tesan@gmail.com>
+ * @version 1.0
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, emulateSdk = 21)
+public class SerleenaDatabaseTest {
+	SerleenaDatabase sh;
+
+	@Test
+	public void testGetWritableDatabase() throws Exception {
+		SQLiteDatabase db = sh.getWritableDatabase();
+	}
+
+	@Before
+	public void setup() throws URISyntaxException {
+		sh = new SerleenaDatabase(RuntimeEnvironment.application, "sample.sqlite", null, 1);
+	}
+}
+
