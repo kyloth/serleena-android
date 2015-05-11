85c285804b768017af735b93101f97e350045639
Filippo Sestini <sestini.filippo@gmail.com>
2015-05-11 14:16:08 +0200

    COMM: Aggiungi ImmutableList
    Related to SHANDROID-161

---
+++ serleena/app/src/main/java/com/kyloth/serleena/common/ImmutableList.java
@@ -0,0 +1,71 @@
+///////////////////////////////////////////////////////////////////////////////
+// 
+// This file is part of Serleena.
+// 
+// The MIT License (MIT)
+//
+// Copyright (C) 2015 Antonio Cavestro, Gabriele Pozzan, Matteo Lisotto, 
+//   Nicola Mometto, Filippo Sestini, Tobia Tesan, Sebastiano Valle.    
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * Name: ImmutableList.java
+ * Package: com.kyloth.serleena.common
+ * Author: Filippo Sestini
+ * Date: 2015-05-11
+ *
+ * History:
+ * Version  Programmer          Date        Changes
+ * 1.0.0    Filippo Sestini     2015-05-11  Creazione file e scrittura di codice
+ *                                          e documentazione in Javadoc.
+ */
+
+package com.kyloth.serleena.common;
+
+/**
+ * Rappresenta una lista immutabile di oggetti.
+ *
+ * Gli oggetti della lista sono ordinati e accessibili casualmente dato
+ * l'indice dell'elemento.
+ *
+ * @author Filippo Sestini <sestini.filippo@gmail.com>
+ * @version 1.0.0
+ */
+public interface ImmutableList<T> extends Iterable<T> {
+
+    /**
+     * Restituisce il numero di elementi della lista.
+     *
+     * @return Numero di elementi della lista.
+     */
+    public int size();
+
+    /**
+     * Restituisce l'elemento della lista alla posizione specificata.
+     *
+     * @param index Indice dell'elemento.
+     * @return Elemento all'indica specificato.
+     */
+    public T get(int index);
+
+}
