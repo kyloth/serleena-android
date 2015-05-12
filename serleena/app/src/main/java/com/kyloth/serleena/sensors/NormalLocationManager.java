1a80b3f16ebeefca4070abecca494a8e54e4fd55
Filippo Sestini <sestini.filippo@gmail.com>
2015-05-12 21:45:15 +0200

    SENS: Aggiungi scheletro di NormalLocationManager
    Related to SHANDROID-167

---
+++ serleena/app/src/main/java/com/kyloth/serleena/sensors/NormalLocationManager.java
@@ -0,0 +1,69 @@
+///////////////////////////////////////////////////////////////////////////////
+// 
+// This file is part of Serleena.
+// 
+// The MIT License (MIT)
+//
+// Copyright (C) 2015 Antonio Cavestro, Gabriele Pozzan, Matteo Lisotto, 
+//   Nicola Mometto, Filippo Sestini, Tobia Tesan, Sebastiano Valle.    
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * Name: NormalLocationManager.java
+ * Package: com.kyloth.serleena.sensors
+ * Author: Filippo Sestini
+ * Date: 2015-05-12
+ *
+ * History:
+ * Version  Programmer       Date        Changes
+ * 1.0.0    Filippo Sestini  2015-05-12  Creazione file e scrittura
+ *                                       codice e documentazione Javadoc
+ */
+
+package com.kyloth.serleena.sensors;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.kyloth.serleena.common.GeoPoint;
+import com.kyloth.serleena.common.UnregisteredObserverException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Concretizza ILocationManager.
+ *
+ * Fornisce al codice client la posizione dell'utente utilizzando il modulo
+ * GPS del dispositivo con il supporto delle API Android.
+ *
+ * @author Filippo Sestini <sestini.filippo@gmail.com>
+ * @version 1.0.0
+ */
+public class NormalLocationManager implements ILocationManager {
+
+}
