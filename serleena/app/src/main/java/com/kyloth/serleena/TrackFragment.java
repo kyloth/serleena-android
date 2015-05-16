c4ef290ede8c7ac2c99f3242cac067f07d803f59
Sebastiano Valle <sebastiano.valle@studenti.unipd.it>
2015-05-16 18:09:04 +0200

    SH: Tolta interfaccia fastidiosa

--- serleena/app/src/main/java/com/kyloth/serleena/TrackFragment.java
+++ serleena/app/src/main/java/com/kyloth/serleena/TrackFragment.java
@@ -42,7 +42,7 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link TrackFragment.OnFragmentInteractionListener} interface
+ * {@link OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link TrackFragment#newInstance} factory method to
  * create an instance of this fragment.
@@ -119,21 +119,6 @@ public class TrackFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
     }
 
 }
