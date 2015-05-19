6b79e386cb68acca4874c88c6999a098f4102319
Filippo Sestini <sestini.filippo@gmail.com>
2015-05-19 12:59:47 +0200

    PRES: Aggiungi struttura di WeatherPresenter
    Related to SHANDROID-92

---
+++ serleena/app/src/main/java/com/kyloth/serleena/presenters/WeatherPresenter.java
@@ -0,0 +1,56 @@
+///////////////////////////////////////////////////////////////////////////////
+// 
+// This file is part of Serleena.
+// 
+// The MIT License (MIT)
+//
+// Copyright (C) 2015 Antonio Cavestro, Gabriele Pozzan, Matteo Lisotto, 
+//   Nicola Mometto, Filippo Sestini, Tobia Tesan, Sebastiano Valle.    
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+package com.kyloth.serleena.presenters;
+
+import android.os.AsyncTask;
+
+import com.kyloth.serleena.common.GeoPoint;
+import com.kyloth.serleena.model.ISerleenaDataSource;
+import com.kyloth.serleena.model.IWeatherForecast;
+import com.kyloth.serleena.model.NoSuchWeatherForecastException;
+import com.kyloth.serleena.presentation.ISerleenaActivity;
+import com.kyloth.serleena.presentation.IWeatherPresenter;
+import com.kyloth.serleena.presentation.IWeatherView;
+import com.kyloth.serleena.sensors.ILocationManager;
+import com.kyloth.serleena.sensors.ILocationObserver;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Concretizza IWeatherPresenter.
+ *
+ * @author Filippo Sestini <sestini.filippo@gmail.com>
+ * @version 1.0.0
+ */
+public class WeatherPresenter implements IWeatherPresenter, ILocationObserver {
+
+}
