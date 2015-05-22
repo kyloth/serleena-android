cc05116bdb0c1e2dbcdf860f84ea94b29c45fee0
Filippo Sestini <sestini.filippo@gmail.com>
2015-05-22 21:48:37 +0200

    MODEL: Aggiungi struttura di SerleenaDataSource
    Related to SHANDROID-27

---
+++ serleena/app/src/main/java/com/kyloth/serleena/model/SerleenaDataSource.java
@@ -0,0 +1,62 @@
+///////////////////////////////////////////////////////////////////////////////
+// 
+// This file is part of Serleena.
+// 
+// The MIT License (MIT)
+//
+// Copyright (C) 2015 Antonio Cavestro, Gabriele Pozzan, Matteo Lisotto, 
+//   Nicola Mometto, Filippo Sestini, Tobia Tesan, Sebastiano Valle.    
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * Name: SerleenaDataSource.java
+ * Package: com.hitchikers.serleena.model
+ * Author: Filippo Sestini
+ * Date: 2015-05-22
+ *
+ * History:
+ * Version    Programmer        Date        Changes
+ * 1.0        Filippo Sestini   2015-05-22  Creazione del file e stesura
+ *                                          della documentazione Javadoc.
+ */
+
+package com.kyloth.serleena.model;
+
+import com.kyloth.serleena.common.EmergencyContact;
+import com.kyloth.serleena.common.GeoPoint;
+import com.kyloth.serleena.common.IQuadrant;
+import com.kyloth.serleena.persistence.IExperienceStorage;
+import com.kyloth.serleena.persistence.IPersistenceDataSource;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Concretizza SerleenaDataSource.
+ *
+ * @author Filippo Sestini <sestini.filippo@gmail.com>
+ * @version 1.0.0
+ */
+public class SerleenaDataSource implements ISerleenaDataSource {
+
+}
