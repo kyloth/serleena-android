976967fa6eb9d68f6fe6bcd1c420e7f2b7ba88e6
Filippo Sestini <sestini.filippo@gmail.com>
2015-06-14 16:00:02 +0200

    MODEL: Modifica TelemetryTest

--- serleena/app/src/test/java/com/kyloth/serleena/model/TelemetryTest.java
+++ serleena/app/src/test/java/com/kyloth/serleena/model/TelemetryTest.java
@@ -45,12 +45,17 @@ import org.junit.Test;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
+import org.mockito.Mockito;
+
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
+import com.android.internal.util.Predicate;
 import com.kyloth.serleena.common.GeoPoint;
 import com.kyloth.serleena.common.TelemetryEvent;
 import com.kyloth.serleena.common.TelemetryEventType;
@@ -66,42 +71,12 @@ import com.kyloth.serleena.persistence.ITelemetryStorage;
 
 public class TelemetryTest {
 
-    ITelemetryStorage storage;
-    LocationTelemetryEvent te1;
-    TelemetryEvent te2;
-    LocationTelemetryEvent te3;
-    GeoPoint gp1;
-    GeoPoint gp2;
-    GeoPoint test_point1;
-    GeoPoint test_point2;
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
-
-    /**
-     * Inizializza i campi dati necessari alla conduzione dei test.
-     */
-    @Before
-    public void initialize() {
-        storage = mock(ITelemetryStorage.class);
-        gp1 = mock(GeoPoint.class);
-        gp2 = mock(GeoPoint.class);
-        te1 = mock(LocationTelemetryEvent.class);
-        when(te1.timestamp()).thenReturn(10);
-        when(te1.getType()).thenReturn(TelemetryEventType.Location);
-        when(te1.location()).thenReturn(gp1);
-        te2 = mock(TelemetryEvent.class);
-        when(te2.timestamp()).thenReturn(20);
-        when(te2.getType()).thenReturn(TelemetryEventType.HeartRate);
-        te3 = mock(LocationTelemetryEvent.class);
-        when(te3.timestamp()).thenReturn(30);
-        when(te3.location()).thenReturn(gp2);
-        when(te3.getType()).thenReturn(TelemetryEventType.Location);
-        when(storage.getEvents()).thenReturn(Arrays.asList(new TelemetryEvent[]
-                                             {te1, te2, te3}));
-        when(gp1.distanceTo(test_point1)).thenReturn(new Float(10));
-        when(gp1.distanceTo(test_point2)).thenReturn(new Float(20));
-        when(gp2.distanceTo(test_point1)).thenReturn(new Float(5));
-        when(gp2.distanceTo(test_point2)).thenReturn(new Float(25));
+    private List<TelemetryEvent> getListFromIterable(Iterable<TelemetryEvent>
+                                                             iterable) {
+        List<TelemetryEvent> list = new ArrayList<>();
+        for (TelemetryEvent e : iterable)
+            list.add(e);
+        return list;
     }
 
     /**
@@ -110,154 +85,94 @@ public class TelemetryTest {
      */
     @Test
     public void getEventsShouldCorrectlyHandleStorageEvents() {
-        Telemetry telemetry = new Telemetry(storage);
-        Iterable<TelemetryEvent> events = telemetry.getEvents();
-        Iterator<TelemetryEvent> i_events = events.iterator();
-        assertTrue(i_events.next() == te1);
-        assertTrue(i_events.next() == te2);
-        assertTrue(i_events.next() == te3);
+        TelemetryEvent te1 = mock(TelemetryEvent.class);
+        TelemetryEvent te2 = mock(TelemetryEvent.class);
+        ITelemetryStorage ts = mock(ITelemetryStorage.class);
+        List<TelemetryEvent> list = new ArrayList<>();
+        list.add(te1);
+        list.add(te2);
+        when(ts.getEvents()).thenReturn(list);
+
+        Telemetry telemetry = new Telemetry(ts);
+        assertTrue(telemetry.getEvents() == ts.getEvents());
     }
 
     /**
-     * Verifica che il metodo getEvents restituisca solo gli eventi i
-     * cui timestamp siano compresi tra i parametri forniti.
+     * Verifica che getEvents() restituisca il risultato corretto.
      */
     @Test
-    public void getEventsShouldReturnEventsBetweenParams()
-    throws NoSuchTelemetryEventException {
-        Telemetry telemetry = new Telemetry(storage);
-        Iterable<TelemetryEvent> events = telemetry.getEvents(15, 25);
-        Iterator<TelemetryEvent> i_events = events.iterator();
-        assertTrue(i_events.next() == te2);
-        assertFalse(i_events.hasNext());
+    public void getEventsWithPredicateShouldWork()
+            throws NoSuchTelemetryEventException {
+        final TelemetryEvent te1 = mock(TelemetryEvent.class);
+        final TelemetryEvent te2 = mock(TelemetryEvent.class);
+        final TelemetryEvent te3 = mock(TelemetryEvent.class);
+        ITelemetryStorage ts = mock(ITelemetryStorage.class);
+        List<TelemetryEvent> list = new ArrayList<>();
+        list.add(te1);
+        list.add(te2);
+        list.add(te3);
+        when(ts.getEvents()).thenReturn(list);
+
+        Predicate<TelemetryEvent> predicate = new Predicate<TelemetryEvent>() {
+            @Override
+            public boolean apply(TelemetryEvent telemetryEvent) {
+                return (telemetryEvent == te2) || (telemetryEvent == te3);
+            }
+        };
+
+        Telemetry telemetry = new Telemetry(ts);
+        List<TelemetryEvent> events = getListFromIterable(
+                telemetry.getEvents(predicate));
+        assertTrue(events.contains(te2));
+        assertTrue(events.contains(te3));
+        assertTrue(events.size() == 2);
     }
 
     /**
-     * Verifica che il metodo getEvents lanci un'eccezione di tipo
-     * NoSuchTelemetryEventException quando non fossero presenti
-     * eventi il cui timestamp sia compreso tra i parametri forniti.
+     * Verifica che getEvents() sollevi NoSuchTelemetryException quando non
+     * vi è alcun evento che soddisfa il predicato specificato.
      */
-    @Test
-    public void getEventsShouldThrowExceptionWhenNoEventsInBoundaries()
-    throws NoSuchTelemetryEventException {
-        Telemetry telemetry = new Telemetry(storage);
-        exception.expect(NoSuchTelemetryEventException.class);
-        Iterable<TelemetryEvent> events = telemetry.getEvents(35, 40);
-    }
+    @Test(expected = NoSuchTelemetryEventException.class)
+    public void getEventsWithPredicateWhenNotFoundShouldThrow()
+            throws NoSuchTelemetryEventException {
+        final TelemetryEvent te1 = mock(TelemetryEvent.class);
+        final TelemetryEvent te2 = mock(TelemetryEvent.class);
+        final TelemetryEvent te3 = mock(TelemetryEvent.class);
+        ITelemetryStorage ts = mock(ITelemetryStorage.class);
+        List<TelemetryEvent> list = new ArrayList<>();
+        list.add(te1);
+        list.add(te2);
+        list.add(te3);
+        when(ts.getEvents()).thenReturn(list);
 
-    /**
-     * Verifica che il metodo getEvents restituisca tutti gli eventi
-     * del tipo TelemetryEventType specificato come parametro.
-     */
-    @Test
-    public void getEventsShouldReturnCorrectTypeEvents()
-    throws NoSuchTelemetryEventException {
-        Telemetry telemetry = new Telemetry(storage);
-        Iterable<TelemetryEvent> events = telemetry.getEvents(TelemetryEventType.Location);
-        Iterator<TelemetryEvent> i_events = events.iterator();
-        assertTrue(i_events.next() == te1);
-        assertTrue(i_events.next() == te3);
-        assertFalse(i_events.hasNext());
-    }
+        Predicate<TelemetryEvent> predicate = new Predicate<TelemetryEvent>() {
+            @Override
+            public boolean apply(TelemetryEvent telemetryEvent) {
+                return false;
+            }
+        };
 
-    /**
-     * Verifica che il metodo getEvents lanci un'eccezione di tipo
-     * NoSuchTelemetryEventException qualora non fossero presenti
-     * eventi del tipo specificato come parametro.
-     */
-    @Test
-    public void getEventsShouldThrowExceptionWhenNoEventsOfType()
-    throws NoSuchTelemetryEventException {
-        Telemetry telemetry = new Telemetry(storage);
-        exception.expect(NoSuchTelemetryEventException.class);
-        Iterable<TelemetryEvent> events = telemetry.getEvents(TelemetryEventType.CheckpointReached);
-    }
-
-    /**
-     * Verifica che il metodo getEventAtLocation lanci un'eccezione
-     * di tipo IllegalArgumentException con messaggio "Illegal null
-     * location" se invocato con GeoPoint nullo.
-     */
-    public void getEventAtLocationShouldThrowExceptionWhenNullGeoPoint()
-    throws NoSuchTelemetryEventException {
-        Telemetry telemetry = new Telemetry(storage);
-        exception.expect(IllegalArgumentException.class);
-        exception.expectMessage("Illegal null location");
-        LocationTelemetryEvent event = telemetry.getEventAtLocation(null, 10);
-    }
-
-    /**
-     * Verifica che il metodo getEventAtLocation restituisca l'evento
-     * la cui distanza sia la più breve e minore della tolleranza specificata
-     * come parametro.
-     */
-    public void getEventAtLocationShouldReturnNearestEvent()
-    throws NoSuchTelemetryEventException {
-        Telemetry telemetry = new Telemetry(storage);
-        LocationTelemetryEvent lte = telemetry.getEventAtLocation(test_point1, 15);
-        assertTrue(lte == te3);
-    }
-
-    /**
-     * Verifica che il metodo getEventAtLocation lanci un'eccezione di
-     * tipo NoSuchTelemetryEventException qualora non vi fossero eventi
-     * la cui distanza sia minore della tolleranza fornita come parametro.
-     */
-    public void getEventAtLocationShouldThrowExceptionWhenTooFarEvents()
-    throws NoSuchTelemetryEventException {
-        Telemetry telemetry = new Telemetry(storage);
-        exception.expect(NoSuchTelemetryEventException.class);
-        LocationTelemetryEvent lte = telemetry.getEventAtLocation(test_point2, 15);
+        new Telemetry(ts).getEvents(predicate);
     }
 
     /**
      * Verifica che il metodo getDuration restituisca il timestamp
      * maggiore tra quelli degli eventi relativi al tracciamento.
      */
+    @Test
     public void getDurationShouldReturnGreatestTimestamp() {
-        Telemetry telemetry = new Telemetry(storage);
-        int duration = telemetry.getDuration();
-        assertTrue(duration == 30);
+        TelemetryEvent te1 = mock(TelemetryEvent.class);
+        when(te1.timestamp()).thenReturn(20);
+        TelemetryEvent te2 = mock(TelemetryEvent.class);
+        when(te2.timestamp()).thenReturn(30);
+        ITelemetryStorage ts = mock(ITelemetryStorage.class);
+        List<TelemetryEvent> list = new ArrayList<>();
+        list.add(te1);
+        list.add(te2);
+        when(ts.getEvents()).thenReturn(list);
+
+        assertTrue(new Telemetry(ts).getDuration() == 30);
     }
 
-    /**
-     * Verifica che il metodo getEvents sollevi un'eccezione di tipo
-     * IllegalArgumentException con messaggio "Illegal time below zero"
-     * se invocato con almeno uno dei due parametri minore di zero.
-     */
-    public void getEventsShouldThrowExceptionWhenParamsBelowZero()
-    throws NoSuchTelemetryEventException {
-        Telemetry telemetry = new Telemetry(storage);
-        exception.expect(IllegalArgumentException.class);
-        exception.expectMessage("Illegal time below zero");
-        telemetry.getEvents(-1, 2);
-        exception.expect(IllegalArgumentException.class);
-        telemetry.getEvents(2, -1);
-    }
-
-    /**
-     * Verifica che il metodo getEvents sollevi un'eccezione di tipo
-     * IllegalArgumentException con messaggio "Illegal time segment"
-     * se invocato con parametro to minore del parametro from.
-     */
-    public void getEventsShouldThrowExceptionWhenIllegalSegment()
-    throws NoSuchTelemetryEventException {
-        Telemetry telemetry = new Telemetry(storage);
-        exception.expect(IllegalArgumentException.class);
-        exception.expectMessage("Illegal time segment");
-        telemetry.getEvents(5, 3);
-    }
-
-    /**
-     * Verifica che il metodo getEventAtLocation sollevi un'eccezione
-     * di tipo IllegalArgumentException con messaggio "Illegal tolerance
-     * below zero" se invocato con tolleranza minore di zero.
-     */
-    public void getEventsAtLocationShouldThrowExceptionWhenNegativeTolerance()
-    throws NoSuchTelemetryEventException {
-        Telemetry telemetry = new Telemetry(storage);
-        exception.expect(IllegalArgumentException.class);
-        exception.expectMessage("Illegal tolerance below zero");
-        telemetry.getEventAtLocation(test_point1, -2);
-    }
 }
+
