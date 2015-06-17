7be5afc04f305e2a6ba9d1ef013f8e1b0e3ff797
Filippo Sestini <sestini.filippo@gmail.com>
2015-06-18 00:18:12 +0200

    FRAG: Aggiungi CompassWidget

---
+++ serleena/app/src/main/java/com/kyloth/serleena/view/widgets/CompassWidget.java
@@ -0,0 +1,79 @@
+///////////////////////////////////////////////////////////////////////////////
+// 
+// This file is part of Serleena.
+// 
+// The MIT License (MIT)
+//
+// Copyright (C) 2015 Antonio Cavestro, Gabriele Pozzan, Matteo Lisotto, 
+//   Nicola Mometto, Filippo Sestini, Tobia Tesan, Sebastiano Valle.    
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+package com.kyloth.serleena.view.widgets;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.kyloth.serleena.R;
+
+public class CompassWidget extends ImageView {
+
+    float direction = 0;
+
+    /**
+     * Crea un nuovo oggetto CompassWidget.
+     */
+    public CompassWidget(Context context) {
+        super(context);
+        this.setImageResource(R.drawable.compass);
+    }
+
+    public CompassWidget(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.setImageResource(R.drawable.compass);
+    }
+
+    public CompassWidget(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        this.setImageResource(R.drawable.compass);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        int height = this.getHeight();
+        int width = this.getWidth();
+
+        canvas.rotate(direction, width / 2, height / 2);
+        super.onDraw(canvas);
+    }
+
+    public void setDirection(float direction) {
+        this.direction = direction;
+        this.invalidate();
+    }
+
+}
