122f7c8efb03300cf49100760c9c958394a83210
Filippo Sestini <sestini.filippo@gmail.com>
2015-06-17 11:06:55 +0200

    FRAG: Riscrivi WeatherFragment

--- serleena/app/src/main/java/com/kyloth/serleena/view/fragments/WeatherFragment.java
+++ serleena/app/src/main/java/com/kyloth/serleena/view/fragments/WeatherFragment.java
@@ -28,179 +28,102 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 
-/**
- * Name: WeatherFragment
- * Package: com.kyloth.serleena.view.fragments
- * Author: Sebastiano Valle
- *
- * History:
- * Version   Programmer         Changes
- * 1.0.0     Sebastiano Valle   Creazione del file, scrittura del codice e di Javadoc
- */
 package com.kyloth.serleena.view.fragments;
 
-import android.app.Activity;
+import android.os.Bundle;
 import android.app.Fragment;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.view.KeyEvent;
-import android.widget.ImageView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.kyloth.serleena.R;
 import com.kyloth.serleena.model.IWeatherForecast;
-import com.kyloth.serleena.persistence.WeatherForecastEnum;
 import com.kyloth.serleena.presentation.IWeatherPresenter;
 import com.kyloth.serleena.presentation.IWeatherView;
 
-import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
 
-/**
- * Classe che implementa la schermata “Meteo”, in cui vengono mostrate informazioni metereologiche
- * relative ad un giorno
- *
- * @use Viene istanziata e utilizzata dall'Activity per la visualizzazione della schermata. Comunica con il Presenter associato attraverso l'interfaccia IWeatherPresenter.
- * @field presenter : IWeatherPresenter presenter collegato a un WeatherFragment
- * @field now : Calendar data da visualizzare
- * @field weatherMap : HashMap<WeatherForecastEnum,Integer> mappa di corrispondenze tra condizioni meteo e immagini da visualizzare
- * @field image : ImageView immagine su cui disegnare le condizioni metereologiche fornite
- * @field tempTxt : TextView casella di testo dove inserire il valore della temperatura fornito
- * @field dayTxt : TextView casella di testo dove visualizzare la data fornita
- * @author Sebastiano Valle <valle.sebastiano93@gmail.com>
- * @version 1.0.0
- * @see android.app.Fragment
- */
-public class WeatherFragment extends Fragment implements IWeatherView {
+public class WeatherFragment extends Fragment
+        implements IWeatherView, View.OnClickListener{
 
-    private IWeatherPresenter presenter;
+    IWeatherPresenter presenter;
 
-    private Calendar now;
+    public WeatherFragment() {
+        presenter = new IWeatherPresenter() {
+            @Override
+            public void advanceDate() { }
+            @Override
+            public void resume() { }
+            @Override
+            public void pause() { }
+        };
+    }
 
-    private HashMap<WeatherForecastEnum,Integer> weatherMap = new HashMap<>();
 
-    private ImageView image;
-    private TextView tempTxt;
-    private TextView dayTxt;
-
-    /**
-     * Questo metodo viene invocato ogni volta che un WeatherFragment viene collegato ad un'Activity.
-     *
-     * @param activity Activity che ha appena terminato una transazione in cui viene aggiunto il corrente Fragment
-     */
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        initFragment();
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup group = (ViewGroup) inflater.inflate(
+                R.layout.fragment_weather, container, false);
+        for (int i = 0; i < group.getChildCount(); i++)
+            group.getChildAt(i).setOnClickListener(this);
+        return group;
     }
 
-    /**
-     * Metodo che inizializza il Fragment, ovvero la mappa e i riferimenti alle View utilizzate.
-     */
-    private void initFragment() {
-        weatherMap.put(WeatherForecastEnum.Cloudy, R.drawable.cloud);
-        weatherMap.put(WeatherForecastEnum.Rainy,R.drawable.rain);
-        weatherMap.put(WeatherForecastEnum.Snowy, R.drawable.snow);
-        weatherMap.put(WeatherForecastEnum.Stormy, R.drawable.storm);
-        weatherMap.put(WeatherForecastEnum.Sunny, R.drawable.sun);
-        image = (ImageView) getActivity().findViewById(R.id.weather_image);
-        dayTxt = (TextView) getActivity().findViewById(R.id.weather_day);
-        tempTxt = (TextView) getActivity().findViewById(R.id.weather_temperature);
-    }
 
-    /**
-     * Metodo utilizzato per collegare un presenter a un WeatherFragment
-     *
-     * @param presenter Presenter a cui viene collegato un WeatherFragment
-     */
     @Override
     public void attachPresenter(IWeatherPresenter presenter) {
         this.presenter = presenter;
     }
 
-    /**
-     * Metodo utilizzato per impostare e visualizzare delle previsioni meteorologiche.
-     *
-     * @param forecast Oggetto contenente delle previsioni
-     */
     @Override
     public void setWeatherInfo(IWeatherForecast forecast) {
-        image.setVisibility(ImageView.VISIBLE);
-        int hour = now.get(Calendar.HOUR_OF_DAY);
-        Integer temp = 0;
-        WeatherForecastEnum weatherId = null;
-        if(hour >= 12 && hour < 20) {
-            weatherId = forecast.getAfternoonForecast();
-            temp = forecast.getAfternoonTemperature();
-        }
-        if(hour < 12 && hour >= 4) {
-            weatherId = forecast.getMorningForecast();
-            temp = forecast.getMorningTemperature();
-        }
-        if(hour < 4 ||  hour >= 20) {
-            weatherId = forecast.getNightForecast();
-            temp = forecast.getNightTemperature();
-        }
-        Bitmap bmp = BitmapFactory.decodeResource(getResources(),weatherMap.get(weatherId));
-        bmp = Bitmap.createScaledBitmap(bmp,130,130,false);
-        image.setImageBitmap(bmp);
-        String day = new Integer(now.get(Calendar.DAY_OF_MONTH)).toString();
-        if(now.get(Calendar.DAY_OF_MONTH) < 10) day = "0" + day;
-        String month = new Integer(now.get(Calendar.MONTH)).toString();
-        if(now.get(Calendar.MONTH) < 10) month = "0" + month;
-        dayTxt.setText(day + "/" + month + "/" + now.get(Calendar.YEAR));
-        tempTxt.setText(temp.toString() + "°C");
+        if (forecast == null)
+            throw new IllegalArgumentException("Illegal null forecast");
     }
 
-    /**
-     * Metodo utilizzato per rimuovere le informazioni visualizzate
-     */
     @Override
     public void clearWeatherInfo() {
-        image.setVisibility(ImageView.INVISIBLE);
-        tempTxt.setText("NESSUNA INFORMAZIONE DISPONIBILE");
-        dayTxt.setText("");
+
     }
 
-    /**
-     * Metodo utilizzato per impostare una data su un WeatherFragment
-     *
-     * @param date Data impostata
-     */
     @Override
     public void setDate(Date date) {
-        now = Calendar.getInstance();
-        now.setTime(date);
+        if (date == null)
+            throw new IllegalArgumentException("Illegal null date");
+        TextView tv = (TextView) getView().findViewById(R.id.weather_date_text);
+        tv.setText(date.toString());
     }
 
-    /**
-     * Metodo che visualizza le previsioni successive.
-     *
-     * @param keyCode tasto premuto
-     * @param event KeyEvent avvenuto
-     */
-    public void keyDown(int keyCode, KeyEvent event) {
-        presenter.advanceDate();
-    }
-
-    /**
-     * Metodo invocato quando il Fragment viene visualizzato.
-     *
-     */
     @Override
     public void onResume() {
         super.onResume();
         presenter.resume();
     }
 
-    /**
-     * Metodo invocato quando il Fragment smette di essere visualizzato.
-     *
-     */
     @Override
     public void onPause() {
         super.onPause();
         presenter.pause();
     }
+
+    /**
+     * Ridefinisce View.OnClickListener.onClick().
+     *
+     * @param v Vista che ha scatenato l'evento.
+     */
+    @Override
+    public void onClick(View v) {
+        presenter.advanceDate();
+    }
+
+    /**
+     * Ridefinisce Object.toString().
+     */
+    @Override
+    public String toString() {
+        return "Meteo";
+    }
+
 }
