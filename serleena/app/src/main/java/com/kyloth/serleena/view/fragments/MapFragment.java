9b0db7c18b10aab537d9ce5dac57af894b29254c
Filippo Sestini <sestini.filippo@gmail.com>
2015-06-17 18:02:58 +0200

    FRAG: Riscrivi MapFragment

--- serleena/app/src/main/java/com/kyloth/serleena/view/fragments/MapFragment.java
+++ serleena/app/src/main/java/com/kyloth/serleena/view/fragments/MapFragment.java
@@ -28,125 +28,101 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 
-/**
- * Name: MapFragment
- * Package: com.kyloth.serleena.view.fragments
- * Author: Sebastiano Valle
- *
- * History:
- * Version   Programmer         Changes
- * 1.0.0     Sebastiano Valle   Creazione del file, scrittura del codice e di Javadoc
- */
-
 package com.kyloth.serleena.view.fragments;
 
-import android.app.Activity;
-import android.app.Fragment;
-import android.graphics.Canvas;
-import android.view.KeyEvent;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kyloth.serleena.R;
 import com.kyloth.serleena.common.GeoPoint;
 import com.kyloth.serleena.common.IQuadrant;
-import com.kyloth.serleena.R;
 import com.kyloth.serleena.common.NoActiveExperienceException;
 import com.kyloth.serleena.common.UserPoint;
 import com.kyloth.serleena.presentation.IMapPresenter;
+import com.kyloth.serleena.presentation.IMapView;
 import com.kyloth.serleena.view.widgets.MapWidget;
 
+import java.util.ArrayList;
+
 /**
- * Classe che implementa la visuale “Mappa” della schermata “Esperienza”
- *
- * Implementa l'interfaccia IMapView, gestendo tutte le esigenze grafiche
- * riguardo alla visuale Mappa nella schermata Esperienza.
- *
- * @use Viene istanziata e utilizzata dall'Activity per la visualizzazione della schermata. Comunica con il Presenter associato attraverso l'interfaccia IMapPresenter.
- * @field mapWidget : MapWidget widget utilizzato per disegnare la mappa
- * @field presenter : IMapPresenter presenter da notificare all'aggiunta del Fragment all'Activity
- * @author Sebastiano Valle <valle.sebastiano93@gmail.com>
- * @version 1.0.0
- * @see android.app.Fragment
- * @see com.kyloth.serleena.presentation.IMapView
+ * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment implements com.kyloth.serleena.presentation.IMapView  {
+public class MapFragment extends Fragment implements IMapView,
+        View.OnClickListener {
 
-    private MapWidget mapWidget;
-
-    private IMapPresenter presenter;
+    MapWidget map;
+    IMapPresenter presenter;
 
     /**
-     * Questo metodo viene invocato ogni volta che un MapFragment viene collegato ad un'Activity.
-     * Al termine di questo metodo, viene invocato il metodo resume() dell'IMapPresenter in ascolto.
-     *
-     * @param activity Activity che ha appena terminato una transazione in cui viene aggiunto il corrente Fragment
+     * Crea un nuovo oggetto MapFragment.
      */
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        mapWidget = (MapWidget) activity.findViewById(R.id.map_image);
+    public MapFragment() {
+        /* Null object pattern */
+        presenter = new IMapPresenter() {
+            @Override
+            public void newUserPoint() throws NoActiveExperienceException { }
+            @Override
+            public void resume() { }
+            @Override
+            public void pause() { }
+        };
     }
 
     /**
-     * Viene impostata la posizione dell'utente ed in seguito visualizzata.
-     *
-     * @param point posizione dell'utente
+     * Ridefinisce Fragment.onCreateView().
      */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onClick(View v) {
+        try {
+            presenter.newUserPoint();
+        } catch (NoActiveExperienceException e) {}
+    }
+
     @Override
     public void setUserLocation(GeoPoint point) {
-        mapWidget.setUserPosition(point);
-        mapWidget.draw(new Canvas());
+        map.setUserPosition(point);
     }
 
-    /**
-     * Metodo con cui viene visualizzato un quadrante.
-     *
-     * @param q Quadrante da visualizzare
-     */
     @Override
     public void displayQuadrant(IQuadrant q) {
-        mapWidget.setQuadrant(q);
-        mapWidget.draw(new Canvas());
+        if (q == null)
+            throw new IllegalArgumentException("Illegal null quadrant");
+        map.setQuadrant(q);
     }
 
-    /**
-     * Metodo con cui vengono visualizzati i Punti Utente sulla mappa
-     *
-     * @param points punti utente da visualizzare
-     */
     @Override
     public void displayUP(Iterable<UserPoint> points) {
-        mapWidget.setUserPoints(points);
-        mapWidget.draw(new Canvas());
+        if (points == null)
+            throw new IllegalArgumentException("Illegal null points");
+        map.setUserPoints(points);
     }
 
-    /**
-     * Metodo per eseguire un'operazione di subscribe relativa ad un IMapPresenter.
-     *
-     * @param presenter oggetto collegato che verrà notificato da questo Fragment
-     */
     @Override
     public void attachPresenter(IMapPresenter presenter) {
+        if (presenter == null)
+            throw new IllegalArgumentException("Illegal null presenter");
         this.presenter = presenter;
     }
 
-    /**
-     * Implementa IMapView.clear().
-     */
     @Override
     public void clear() {
-
-    }
-
-    /**
-     * Metodo che richiede la creazione di un Punto Utente.
-     *
-     * @param keyCode tasto premuto
-     * @param event KeyEvent avvenuto
-     */
-    public void keyDown(int keyCode, KeyEvent event) {
-        try {
-            presenter.newUserPoint();
-        }
-        catch (NoActiveExperienceException e) {}
+        map.setQuadrant(null);
+        map.setUserPoints(null);
+        map.setUserPosition(null);
     }
 
     /**
@@ -168,4 +144,13 @@ public class MapFragment extends Fragment implements com.kyloth.serleena.present
         super.onPause();
         presenter.pause();
     }
+
+    /**
+     * Ridefinisce Object.toString().
+     */
+    @Override
+    public String toString() {
+        return "Mappa";
+    }
+
 }
