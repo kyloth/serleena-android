c4bbad531a95de8cad7eff487f7d689f62ee898b
Filippo Sestini <sestini.filippo@gmail.com>
2015-06-22 20:32:47 +0200

    SENS: Modifica valore LocationReachedManager.LOCATION_RADIUS

--- serleena/app/src/main/java/com/kyloth/serleena/sensors/LocationReachedManager.java
+++ serleena/app/src/main/java/com/kyloth/serleena/sensors/LocationReachedManager.java
@@ -57,7 +57,7 @@ import java.util.Map;
 class LocationReachedManager
         implements ILocationReachedManager, ILocationObserver {
 
-    public static final int LOCATION_RADIUS = 15;
+    public static final int LOCATION_RADIUS = 100;
 
     private final Map<ILocationReachedObserver, GeoPoint> observers;
     private final Map<ILocationReachedObserver, IWakeupObserver> alarms;
