ed812e1a0642687ba6d38bf1678970edbcdebd2a
Filippo Sestini <sestini.filippo@gmail.com>
2015-06-22 19:55:58 +0200

    SENS/TEST: Aggiungi BackgroundLocationManagerTest

---
+++ serleena/app/src/test/java/com/kyloth/serleena/sensors/BackgroundLocationManagerTest.java
@@ -0,0 +1,304 @@
+///////////////////////////////////////////////////////////////////////////////
+// 
+// This file is part of Serleena.
+// 
+// The MIT License (MIT)
+//
+// Copyright (C) 2015 Antonio Cavestro, Gabriele Pozzan, Matteo Lisotto, 
+//   Nicola Mometto, Filippo Sestini, Tobia Tesan, Sebastiano Valle.    
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * Name: BackgroundLocationManagerTest.java
+ * Package: com.kyloth.serleena.sensors
+ * Author: Filippo Sestini
+ *
+ * History:
+ * Version  Programmer        Changes
+ * 1.0.0    Filippo Sestini   Creazione file e scrittura
+ *                            codice e documentazione Javadoc
+ */
+
+package com.kyloth.serleena.sensors;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+import com.kyloth.serleena.common.GeoPoint;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Contiene i test di unità per la classe BackgroundLocationManager.
+ *
+ * @author Filippo Sestini <sestini.filippo@gmail.com>
+ * @version 1.0.0
+ */
+@RunWith(RobolectricTestRunner.class)
+public class BackgroundLocationManagerTest {
+
+    Context context;
+    Context contextMock;
+    AlarmManager alarmManager;
+    BackgroundLocationManager manager;
+    BackgroundLocationManager managerWithContextMock;
+    ILocationObserver o1;
+    ILocationObserver o2;
+    ILocationObserver o3;
+
+    @Before
+    public void initialize() {
+        o1 = mock(ILocationObserver.class);
+        o2 = mock(ILocationObserver.class);
+        o3 = mock(ILocationObserver.class);
+        contextMock = mock(Context.class);
+        context = RuntimeEnvironment.application;
+        alarmManager = mock(AlarmManager.class);
+        manager = new BackgroundLocationManager(context, alarmManager);
+        managerWithContextMock = new BackgroundLocationManager(contextMock,
+                alarmManager);
+    }
+
+    /**
+     * Verifica che il costruttore sollevi un'eccezione
+     * IllegalArgumentException se vengono passati parametri null.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void ctorShouldThrowWhenNullArgument1() {
+        new BackgroundLocationManager(null, alarmManager);
+    }
+
+    /**
+     * Verifica che il costruttore sollevi un'eccezione
+     * IllegalArgumentException se vengono passati parametri null.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void ctorShouldThrowWhenNullArgument2() {
+        new BackgroundLocationManager(context, null);
+    }
+
+    /**
+     * Verifica che l'intervallo di aggiornamento venga calcolato in base
+     * agli intervalli del vari observer registrati all'oggetto. In
+     * particolare, l'intervallo deve essere pari a quello dell'observer di
+     * intervallo minimo.
+     */
+    @Test
+    public void intervalShouldBeSetAccordingToObservers()
+            throws BackgroundLocationManager.NoObserversException {
+        manager.attachObserver(o2, 40);
+        assertTrue(manager.interval() == 40);
+        manager.attachObserver(o3, 20);
+        assertTrue(manager.interval() == 20);
+        manager.attachObserver(o1, 60);
+        assertTrue(manager.interval() == 20);
+        manager.detachObserver(o3);
+        assertTrue(manager.interval() == 40);
+    }
+
+    /**
+     * Verifica che attachObserver() comporti la registrazione di un'allarme
+     * con l'intervallo minimo tra tutti gli observer.
+     */
+    @Test
+    public void managerShouldRegisterToAlarmManagerWithRightInterval() {
+        manager.attachObserver(mock(ILocationObserver.class), 60);
+        verify(alarmManager).setInexactRepeating(
+                any(Integer.class),
+                any(Long.class),
+                eq(60000L),
+                any(PendingIntent.class)
+        );
+        manager.attachObserver(mock(ILocationObserver.class), 40);
+        verify(alarmManager).cancel(any(PendingIntent.class));
+        verify(alarmManager).setInexactRepeating(
+                any(Integer.class),
+                any(Long.class),
+                eq(40000L),
+                any(PendingIntent.class)
+        );
+        manager.attachObserver(mock(ILocationObserver.class), 20);
+        verify(alarmManager, times(2)).cancel(any(PendingIntent.class));
+        verify(alarmManager).setInexactRepeating(
+                any(Integer.class),
+                any(Long.class),
+                eq(20000L),
+                any(PendingIntent.class)
+        );
+    }
+
+    /**
+     * Verifica che l'istanza registri se stessa come BroadcastReceiver
+     * solamente se vi sono observer registrati.
+     */
+    @Test
+    public void receiverShouldBeRegisteredOnlyWithObservers() {
+        ILocationObserver o1 = mock(ILocationObserver.class);
+        ILocationObserver o2 = mock(ILocationObserver.class);
+        ILocationObserver o3 = mock(ILocationObserver.class);
+
+        managerWithContextMock.attachObserver(o1, 60);
+        verify(contextMock, times(1)).registerReceiver(
+                eq(managerWithContextMock), any(IntentFilter.class));
+        managerWithContextMock.attachObserver(o2, 40);
+        verify(contextMock, times(1)).registerReceiver(
+                eq(managerWithContextMock), any(IntentFilter.class));
+        managerWithContextMock.attachObserver(o3, 20);
+        verify(contextMock, times(1)).registerReceiver(
+                eq(managerWithContextMock), any(IntentFilter.class));
+        managerWithContextMock.detachObserver(o1);
+        verify(contextMock, times(1)).registerReceiver(
+                eq(managerWithContextMock), any(IntentFilter.class));
+        managerWithContextMock.detachObserver(o2);
+        verify(contextMock, times(1)).registerReceiver(
+                eq(managerWithContextMock), any(IntentFilter.class));
+        managerWithContextMock.detachObserver(o3);
+        verify(contextMock, times(1)).registerReceiver(
+                eq(managerWithContextMock), any(IntentFilter.class));
+        verify(contextMock, times(1)).unregisterReceiver(managerWithContextMock);
+    }
+
+    /**
+     * Verifica che attachObserver() sollevi un'eccezione al passaggio di
+     * parametri null.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void attachObserverShouldThrowWhenNullArgument() {
+        manager.attachObserver(null, 20);
+    }
+
+    /**
+     * Verifica che detachObserver() sollevi un'eccezione al passaggio di
+     * parametri null.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void detachObserverShouldThrowWhenNullArgument() {
+        manager.detachObserver(null);
+    }
+
+    /**
+     * Verifica che attachObserver() sollevi un'eccezione quando viene
+     * passato un intervallo pari a 0.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void attachObserverShouldThrowWhenNullInterval() {
+        manager.attachObserver(mock(ILocationObserver.class), 0);
+    }
+
+    /**
+     * Verifica che attachObserver() sollevi un'eccezione quando viene
+     * passato un intervallo negativo.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void attachObserverShouldThrowWhenNegativeInterval() {
+        manager.attachObserver(mock(ILocationObserver.class), -20);
+    }
+
+    /**
+     * Verifica che il metodo notifyObservers() notifichi correttamente tutti
+     * gli observer registrati all'oggetto.
+     */
+    @Test
+    public void notifyObserversShouldNotifyAllObservers() {
+        ILocationObserver o1 = mock(ILocationObserver.class);
+        ILocationObserver o2 = mock(ILocationObserver.class);
+        ILocationObserver o3 = mock(ILocationObserver.class);
+
+        Bundle b = new Bundle();
+        b.putDouble("latitude", 40d);
+        b.putDouble("longitude", 50d);
+        manager.onReceiveResult(0, b);
+
+        manager.attachObserver(o1, 60);
+        manager.attachObserver(o2, 60);
+        manager.attachObserver(o3, 60);
+
+        manager.notifyObservers();
+        verify(o1).onLocationUpdate(any(GeoPoint.class));
+        verify(o2).onLocationUpdate(any(GeoPoint.class));
+        verify(o3).onLocationUpdate(any(GeoPoint.class));
+    }
+
+    /**
+     * Verifica che il metodo onReceiveResult() notifichi gli observer con i
+     * dati sulla posizione comunicati al metodo stesso.
+     */
+    @Test
+    public void onReceiveResultShouldNotifyObserversWithReceivedGeoPoint() {
+        ILocationObserver o = mock(ILocationObserver.class);
+        manager.attachObserver(o, 60);
+
+        Bundle b = new Bundle();
+        b.putDouble("latitude", 40d);
+        b.putDouble("longitude", 50d);
+        manager.onReceiveResult(0, b);
+
+        verify(o).onLocationUpdate(eq(new GeoPoint(40, 50)));
+    }
+
+    /**
+     * Verifica che gli observer non vengano notificati se non è stato ancora
+     * ricevuto alcun dato sulla posizione.
+     */
+    @Test
+    public void notifyObserversShouldNotNotifyIfNoLocation() {
+        ILocationObserver o1 = mock(ILocationObserver.class);
+        ILocationObserver o2 = mock(ILocationObserver.class);
+        ILocationObserver o3 = mock(ILocationObserver.class);
+
+        manager.attachObserver(o1, 60);
+        manager.attachObserver(o2, 60);
+        manager.attachObserver(o3, 60);
+
+        manager.notifyObservers();
+        verify(o1, never()).onLocationUpdate(any(GeoPoint.class));
+        verify(o2, never()).onLocationUpdate(any(GeoPoint.class));
+        verify(o3, never()).onLocationUpdate(any(GeoPoint.class));
+    }
+
+    /**
+     * Verifica che il metodo interval() sollevi un'eccezione quando non vi
+     * sono observer registrati all'oggetto.
+     */
+    @Test(expected = BackgroundLocationManager.NoObserversException.class)
+    public void intervalShouldThrowWhenNoObservers()
+            throws BackgroundLocationManager.NoObserversException {
+        manager.interval();
+    }
+
+}
