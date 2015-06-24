857585257bbfa79283e41bd660ca82d6306bc76d
Filippo Sestini <sestini.filippo@gmail.com>
2015-06-24 12:27:17 +0200

    FRAG/TEST: Modifica imports in ExperienceSelectionFragmentTest

--- serleena/app/src/test/java/com/kyloth/serleena/view/fragments/ExperienceSelectionFragmentTest.java
+++ serleena/app/src/test/java/com/kyloth/serleena/view/fragments/ExperienceSelectionFragmentTest.java
@@ -42,30 +42,12 @@
 package com.kyloth.serleena.view.fragments;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
-import android.content.Intent;
-import android.test.ActivityInstrumentationTestCase2;
-import android.test.ActivityUnitTestCase;
-import android.test.ServiceTestCase;
-import android.view.KeyEvent;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import com.kyloth.serleena.R;
 import com.kyloth.serleena.BuildConfig;
-import com.kyloth.serleena.model.ITrack;
 import com.kyloth.serleena.presentation.IExperienceSelectionPresenter;
-import com.kyloth.serleena.presentation.IExperienceSelectionView;
 import com.kyloth.serleena.presenters.ISerleenaActivity;
-import com.kyloth.serleena.presenters.SerleenaActivity;
-import com.kyloth.serleena.view.fragments.ExperienceSelectionFragment;
-import com.kyloth.serleena.model.IExperience;
 import com.kyloth.serleena.model.ISerleenaDataSource;
-import com.kyloth.serleena.model.ITrack;
 import com.kyloth.serleena.sensors.ISensorManager;
 
 import junit.framework.Assert;
@@ -73,27 +55,14 @@ import junit.framework.Assert;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
-import org.robolectric.shadows.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.Before;
-import org.junit.After;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
-import org.w3c.dom.Text;
-
-import java.lang.Exception;
-import java.lang.Override;
-import java.lang.Throwable;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Date;
-
-import dalvik.annotation.TestTarget;
 
 import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
 
 /**
  * Contiene i test di unità per la classe ExperienceSelectionFragment.
