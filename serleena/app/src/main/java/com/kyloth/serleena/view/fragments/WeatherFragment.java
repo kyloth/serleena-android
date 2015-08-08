4695b2cf598f228aa1439b269e1821bc6f7b89fe
Filippo Sestini <sestini.filippo@gmail.com>
2015-08-08 23:45:00 +0200

    FRAG: Aggiungi logica a WeatherFragment

--- serleena/app/src/main/java/com/kyloth/serleena/view/fragments/WeatherFragment.java
+++ serleena/app/src/main/java/com/kyloth/serleena/view/fragments/WeatherFragment.java
@@ -41,15 +41,44 @@ import com.kyloth.serleena.R;
 import com.kyloth.serleena.model.IWeatherForecast;
 import com.kyloth.serleena.presentation.IWeatherPresenter;
 import com.kyloth.serleena.presentation.IWeatherView;
+import com.kyloth.serleena.view.widgets.WeatherWidget;
 
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WeatherFragment extends Fragment
         implements IWeatherView, View.OnClickListener{
 
-    IWeatherPresenter presenter;
+    private IWeatherPresenter presenter;
+    private TextView dateText;
+    private TextView noInfoText;
+    private TextView morningTempText;
+    private TextView afternoonTempText;
+    private TextView nightTempText;
+    private WeatherWidget morningWidget;
+    private WeatherWidget afternoonWidget;
+    private WeatherWidget nightWidget;
+    private Map<Integer, String> monthNames;
 
     public WeatherFragment() {
+        monthNames = new HashMap<>();
+        monthNames.put(1, "Gennaio");
+        monthNames.put(2, "Febbraio");
+        monthNames.put(3, "Marzo");
+        monthNames.put(4, "Aprile");
+        monthNames.put(5, "Maggio");
+        monthNames.put(6, "Giugno");
+        monthNames.put(7, "Luglio");
+        monthNames.put(8, "Agosto");
+        monthNames.put(9, "Settembre");
+        monthNames.put(10, "Ottobre");
+        monthNames.put(11, "Novembre");
+        monthNames.put(12, "Dicembre");
+
         presenter = new IWeatherPresenter() {
             @Override
             public void advanceDate() { }
@@ -60,17 +89,35 @@ public class WeatherFragment extends Fragment
         };
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup group = (ViewGroup) inflater.inflate(
                 R.layout.fragment_weather, container, false);
+
+        dateText = (TextView) group.findViewById(R.id.weather_date_text);
+        noInfoText = (TextView) group.findViewById(R.id.weather_no_info);
+        morningWidget = (WeatherWidget) group.findViewById(R.id.morning_widget);
+        afternoonWidget =
+                (WeatherWidget) group.findViewById(R.id.afternoon_widget);
+        nightWidget = (WeatherWidget) group.findViewById(R.id.night_widget);
+        morningTempText = (TextView) group.findViewById(R.id.morning_temp_text);
+        afternoonTempText =
+                (TextView) group.findViewById(R.id.afternoon_temp_text);
+        nightTempText = (TextView) group.findViewById(R.id.night_temp_text);
+
         for (int i = 0; i < group.getChildCount(); i++)
             group.getChildAt(i).setOnClickListener(this);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        String text = calendar.get(Calendar.DAY_OF_MONTH) + " " + monthNames
+                .get(calendar.get(Calendar.MONTH)) + " " + calendar.get
+                (Calendar.YEAR);
+        dateText.setText(text);
+
         return group;
     }
-
 
     @Override
     public void attachPresenter(IWeatherPresenter presenter) {
@@ -81,19 +128,39 @@ public class WeatherFragment extends Fragment
     public void setWeatherInfo(IWeatherForecast forecast) {
         if (forecast == null)
             throw new IllegalArgumentException("Illegal null forecast");
+
+        noInfoText.setVisibility(View.INVISIBLE);
+        morningWidget.setVisibility(View.VISIBLE);
+        afternoonWidget.setVisibility(View.VISIBLE);
+        nightWidget.setVisibility(View.VISIBLE);
+        morningTempText.setVisibility(View.VISIBLE);
+        afternoonTempText.setVisibility(View.VISIBLE);
+        nightTempText.setVisibility(View.VISIBLE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(forecast.date().getTime());
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        dateText.setText(day + " " + monthNames.get(month) + " " + year);
+        morningWidget.setCondition(forecast.getMorningForecast());
+        afternoonWidget.setCondition(forecast.getAfternoonForecast());
+        nightWidget.setCondition(forecast.getNightForecast());
+        morningTempText.setText(forecast.getMorningTemperature() + " °C");
+        afternoonTempText.setText(forecast.getAfternoonTemperature() + " °C");
+        nightTempText.setText(forecast.getNightTemperature() + " °C");
     }
 
     @Override
     public void clearWeatherInfo() {
-
-    }
-
-    @Override
-    public void setDate(Date date) {
-        if (date == null)
-            throw new IllegalArgumentException("Illegal null date");
-        TextView tv = (TextView) getView().findViewById(R.id.weather_date_text);
-        tv.setText(date.toString());
+        noInfoText.setVisibility(View.VISIBLE);
+        morningWidget.setVisibility(View.INVISIBLE);
+        afternoonWidget.setVisibility(View.INVISIBLE);
+        nightWidget.setVisibility(View.INVISIBLE);
+        morningTempText.setVisibility(View.INVISIBLE);
+        afternoonTempText.setVisibility(View.INVISIBLE);
+        nightTempText.setVisibility(View.INVISIBLE);
     }
 
     @Override
