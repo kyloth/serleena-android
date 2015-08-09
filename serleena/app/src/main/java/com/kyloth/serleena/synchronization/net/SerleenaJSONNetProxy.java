b7e0a5846eb1c6f3bf1b8a8e7c2e88f6b0d53190
Tobia Tesan <tobia.tesan@gmail.com>
2015-08-09 13:59:28 +0200

    SYNC: Aggiungi SerleenaJSONNetProxy
    Related to: SHANDROID-260

--- serleena/app/src/main/java/com/kyloth/serleena/synchronization/net/SerleenaJSONNetProxy.java
+++ serleena/app/src/main/java/com/kyloth/serleena/synchronization/net/SerleenaJSONNetProxy.java
@@ -41,9 +41,20 @@ package com.kyloth.serleena.synchronization.net;
 
 import com.kyloth.serleena.synchronization.AuthException;
 import com.kyloth.serleena.synchronization.InboundStream;
-import com.kyloth.serleena.synchronization.OutboundStream;
+import com.kyloth.serleena.synchronization.kylothcloud.IKylothIdSource;
+import com.kyloth.serleena.synchronization.kylothcloud.inbound.CloudJSONInboundStream;
+import com.kyloth.serleena.synchronization.kylothcloud.outbound.CloudJSONOutboundStream;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Scanner;
 
 /**
  * Concretizza l'interfaccia INetProxy permettendo di dialogare con
@@ -53,67 +64,219 @@ import java.io.IOException;
  *        per poter dialogare con il servizio KylothCloud utilizzando le
  *        primitive ad alto livello prescritte da INetProxy.
  * @author Tobia Tesan <tobia.tesan@gmail.com>
- * @field url L'URL del servizio remoto
+ * @field baseUrl L'URL del servizio remoto
  * @field auth_token Il token di autorizzazione ricevuto dal servizio remoto al termine
  *                   della procedura di handshaking, valido per l'intera vita
  *                   dell'oggetto.
  */
 public class SerleenaJSONNetProxy implements INetProxy {
-	String url;
-	String auth_token;
+    URL baseUrl;
+    String authToken;
+    String tempToken;
+    HttpURLConnection urlConnection;
+    IKylothIdSource kylothIdSource;
+    SerleenaConnectionFactory factory;
 
-	/**
-	 * Costruisce un'istanza di SerleenaJSONNetProxy
-	 * @param url L'URL del servizio remoto
-	 */
-	SerleenaJSONNetProxy(String url) {
-		this.url = url;
-	}
+    final String AUTH_TOKEN_NAME = "X-AuthToken";
+    final String DATA_TOKEN_NAME = "Data";
+    final String CHARSET = "UTF-8";
 
-	/**
-	 * Fornisce un OutboundStream in cui scrivere i dati raccolti localmente
-	 */
-	@Override
-	public OutboundStream send() throws AuthException, IOException {
-		// TODO
-		return null;
-	}
+    public String getCharset() {
+        return CHARSET;
+    }
+    private String getTempToken() {
+        return tempToken;
+    }
 
-	/**
-	 * Richiede i dati di sincronizzazione dal servizio remoto.
-	 *
-	 * @return Un InboundStream contenente i dati in forma grezza provenienti
-	 *         dal servizio remoto.
-	 */
-	@Override
-	public InboundStream get() {
-		//TODO
-		return null;
-	}
+    private URL getAuthUrl() {
+        URL authUrl = null;
 
-	/**
-	 * Richiede la preautorizzazione con il servizio remoto.
-	 *
-	 * @return La stringa con il token temporaneo da visualizzare che
-	 *         l'utente dovra' poi confermare sull'interfaccia cloud..
-	 */
-	@Override
-	public String preAuth() {
-		//TODO
-		return null;
-	}
+        try {
+            authUrl = new URL(baseUrl.toString()+"/users/pair/"+getTempToken());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 
-	/**
-	 * Richiede di eseguire la procedura di autorizzazione permanente contro
-	 * il servizio remoto.
-	 */
-	@Override
-	public void auth() {
-		//TODO
-	}
+        return authUrl;
+    }
 
-	@Override
-	public void disconnect() {
-		// TODO
-	}
+    private URL getSyncUrl() {
+        URL syncUrl = null;
+
+        try {
+            syncUrl = new URL(baseUrl.toString()+"/users/sync/"+kylothIdSource.getKylothId());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return syncUrl;
+    }
+
+    private URL getPreAuthUrl() {
+
+        URL preAuthUrl = null;
+
+        try {
+            preAuthUrl = new URL(baseUrl.toString()+"/tokens/"+kylothIdSource.getKylothId());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return preAuthUrl;
+    }
+
+
+    private void connect() throws AuthException, IOException {
+        if (authToken == null) {
+            throw new AuthException("Not paired yet?");
+        } else {
+            urlConnection = factory.createURLConnection(getSyncUrl());
+            urlConnection.addRequestProperty(AUTH_TOKEN_NAME, authToken);
+            urlConnection.setDoInput(true);
+            urlConnection.setDoOutput(true);
+        }
+    }
+
+    @Override
+    public void disconnect() {
+        if (urlConnection == null) {
+            throw new RuntimeException("No urlConnection?");
+        } else {
+            try {
+                urlConnection.disconnect();
+            } finally {
+                urlConnection = null;
+            }
+        }
+    }
+    /**
+     * Costruisce un'istanza di SerleenaJSONNetProxy
+     * @param baseUrl L'URL del servizio remoto
+     */
+    public SerleenaJSONNetProxy(IKylothIdSource kylothIdSource, URL baseUrl) {
+        this.kylothIdSource = kylothIdSource;
+        this.baseUrl = baseUrl;
+
+        factory = new SerleenaConnectionFactory() {
+            public HttpURLConnection createURLConnection(URL url) throws IOException{
+                return (HttpURLConnection) url.openConnection();
+            }
+        };
+    }
+
+    /**
+     * Costruisce un'istanza di SerleenaJSONNetProxy
+     * @param baseUrl L'URL del servizio remoto
+     * @param factory Factory che restituirà l'oggeto urlConnection
+     */
+    public SerleenaJSONNetProxy(URL baseUrl, IKylothIdSource kylothIdSource, SerleenaConnectionFactory factory) {
+        this.baseUrl = baseUrl;
+        this.factory = factory;
+        this.kylothIdSource = kylothIdSource;
+    }
+
+    /**
+     * Ottieni un OutboundStream per scrivere i dati raccolti localmente.
+     *
+     */
+    @Override
+    public CloudJSONOutboundStream send() throws AuthException, IOException {
+        if (urlConnection == null) {
+            connect();
+            urlConnection.setRequestMethod("POST");
+            OutputStream os = urlConnection.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, CHARSET));
+            writer.write(URLEncoder.encode(DATA_TOKEN_NAME, CHARSET) + "=");
+            writer.flush();
+            return new CloudJSONOutboundStream(os);
+        } else {
+            throw new RuntimeException("Existing urlConnection. send() must be called after auth sequence, before get()");
+        }
+    }
+
+    /**
+     * Richiede i dati di sincronizzazione dal servizio remoto.
+     *
+     * @return Un InboundStream contenente i dati in forma grezza provenienti
+     *         dal servizio remoto.
+     */
+    @Override
+    public InboundStream get() throws AuthException, IOException {
+        if (urlConnection == null) {
+            connect();
+            urlConnection.setRequestMethod("GET");
+            if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                CloudJSONInboundStream in;
+                in = new CloudJSONInboundStream(urlConnection.getInputStream());
+                return in;
+            } else {
+                int c = urlConnection.getResponseCode();
+                disconnect();
+                throw new AuthException("Got " + c + " from remote service");
+            }
+        } else {
+            throw new RuntimeException("Existing URL connection? disconnect() first");
+        }
+    }
+
+    /**
+     * Richiede la preautorizzazione con il servizio remoto.
+     *
+     * @return La stringa con il token temporaneo da visualizzare che
+     *         l'utente dovra' poi confermare sull'interfaccia cloud..
+     */
+    @Override
+    public String preAuth() throws AuthException, IOException {
+        if (urlConnection != null) {
+            throw new RuntimeException("Existing urlConnection. Looks like send() or get() attempted. preAuth() must be called first.");
+        } else {
+            authToken = null;
+            HttpURLConnection tempUrlConnection = factory.createURLConnection(getPreAuthUrl());
+
+            if (tempUrlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                String contentType = tempUrlConnection.getContentType();
+                if (contentType.contains("text/plain")) {
+                    InputStream stream = tempUrlConnection.getInputStream();
+                    Scanner s = new Scanner(stream).useDelimiter("\\A");
+                    String out = s.hasNext() ? s.next() : "";
+                    tempToken = out.trim();
+                } else {
+                    throw new IOException("Content type was" + contentType);
+                }
+            } else {
+                throw new AuthException("Got " + tempUrlConnection.getResponseCode() + " from remote service");
+            }
+            tempUrlConnection.disconnect();
+            return tempToken;
+        }
+    }
+
+    /**
+     * Richiede di eseguire la procedura di autorizzazione permanente contro
+     * il servizio remoto.
+     */
+    @Override
+    public void auth() throws AuthException, IOException {
+        authToken = null;
+        if (urlConnection != null) {
+            throw new RuntimeException("Existing urlConnection. Looks like send() or get() attempted. auth() must be called first.");
+        } else {
+            HttpURLConnection tempUrlConnection = factory.createURLConnection(getAuthUrl());
+            if (tempUrlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                String contentType = tempUrlConnection.getContentType();
+                if (contentType.contains("text/plain")) {
+                    InputStream stream = tempUrlConnection.getInputStream();
+                    Scanner s = new Scanner(stream).useDelimiter("\\A");
+                    String out = s.hasNext() ? s.next() : "";
+                    authToken = out.trim();
+                    tempToken = null;
+                } else {
+                    throw new IOException("Content type was" + contentType);
+                }
+            } else {
+                throw new AuthException("Got " + tempUrlConnection.getResponseCode() + " from remote service");
+            }
+        }
+    }
 }
