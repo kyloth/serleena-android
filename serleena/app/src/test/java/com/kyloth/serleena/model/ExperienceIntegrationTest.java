e0f733bc4dedb6802d77ac67ea686470f9fa89fb
Filippo Sestini <sestini.filippo@gmail.com>
2015-08-13 17:17:32 +0200

    MODEL/TEST: Rinomina ExperienceTest in ExperienceIntegrationTest

---
+++ serleena/app/src/test/java/com/kyloth/serleena/model/ExperienceIntegrationTest.java
@@ -0,0 +1,170 @@
+///////////////////////////////////////////////////////////////////////////////
+// 
+// This file is part of Serleena.
+// 
+// The MIT License (MIT)
+//
+// Copyright (C) 2015 Antonio Cavestro, Gabriele Pozzan, Matteo Lisotto, 
+//   Nicola Mometto, Filippo Sestini, Tobia Tesan, Sebastiano Valle.    
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * Name: ExperienceIntegrationTest.java
+ * Package: com.kyloth.serleena.presenters;
+ * Author: Gabriele Pozzan
+ *
+ * History:
+ * Version  Programmer       Changes
+ * 1.0.0    Gabriele Pozzan  Creazione file scrittura
+ *                                       codice e documentazione Javadoc
+ */
+
+package com.kyloth.serleena.model;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.Iterator;
+
+import com.kyloth.serleena.BuildConfig;
+import com.kyloth.serleena.TestDB;
+import com.kyloth.serleena.persistence.sqlite.SerleenaSQLiteDataSource;
+import com.kyloth.serleena.persistence.sqlite.SerleenaDatabase;
+import com.kyloth.serleena.common.UserPoint;
+
+/**
+ * Contiene test di integrazione per la classe Experience.
+ *
+ * @author Gabriele Pozzan <gabriele.pozzan@studenti.unipd.it>
+ * @version 1.0.0
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, emulateSdk = 19)
+public class ExperienceIntegrationTest {
+
+    private SQLiteDatabase db;
+    private SerleenaDatabase serleenaDB;
+    private SerleenaDataSource dataSource;
+
+    /**
+     * Inizializza i campi dati necessari alla conduzione dei test.
+     */
+    @Before
+    public void initialize() {
+        serleenaDB = new SerleenaDatabase(RuntimeEnvironment.application, 1);
+        db = serleenaDB.getWritableDatabase();
+        dataSource = new SerleenaDataSource(
+                new SerleenaSQLiteDataSource(
+                        RuntimeEnvironment.application, serleenaDB));
+        TestDB.experienceQuery(db, 0, "experience");
+    }
+
+    /**
+     * Verifica che il metodo getName restituisca il nome effettivo
+     * delle Esperienze salvate nel db.
+     */
+    @Test
+    public void testGetName() {
+        IExperience experience = dataSource.getExperiences().iterator().next();
+        assertEquals("experience", experience.getName());
+    }
+
+    /**
+     * Verifica che il metodo toString() restituisca il nome dell'Esperienza.
+     */
+    @Test
+    public void toStringShouldReturnNameOfExperience() {
+        IExperience experience = dataSource.getExperiences().iterator().next();
+        assertEquals("experience", experience.toString());
+    }
+
+    /**
+     * Verifica che il metodo getUserPoints restituisca i corretti
+     * Punti Utente per le diverse Esperienze.
+     */
+    @Test
+    public void testGetUserPoints() {
+        TestDB.userPointQuery(db, 0, 5, 5, 0);
+        TestDB.userPointQuery(db, 1, 10, 10, 0);
+
+        IExperience experience = dataSource.getExperiences().iterator().next();
+        Iterator<UserPoint> iterator = experience.getUserPoints().iterator();
+        UserPoint up1 = iterator.next();
+        UserPoint up2 = iterator.next();
+
+        assertTrue(!iterator.hasNext());
+
+        boolean b1, b2;
+        b1 = up1.latitude() == 5 && up1.longitude() == 5 &&
+                up2.latitude() == 10 && up2.longitude() == 10;
+        b2 = up1.latitude() == 10 && up1.longitude() == 10 &&
+                up2.latitude() == 5 && up2.longitude() == 5;
+        assertTrue(b1 || b2);
+    }
+
+    /**
+     * Verifica che il metodo addUserPoints aggiunga un Punto
+     * Utente per l'Esperienza sul quale è chiamato.
+     */
+    @Test
+    public void testAddUserPoints() {
+        IExperience experience = dataSource.getExperiences().iterator().next();
+        experience.addUserPoints(new UserPoint(5, 10));
+
+        Iterator<UserPoint> iterator = experience.getUserPoints().iterator();
+        UserPoint up = iterator.next();
+
+        assertTrue(!iterator.hasNext());
+        assertTrue(5.0 == up.latitude());
+        assertTrue(10.0 == up.longitude());
+    }
+
+    /**
+     * Verifica che il metodo getTracks restituisca correttamente
+     * i Percorsi per una determinata Esperienza.
+     */
+    @Test
+    public void testGetTracks() {
+        TestDB.experienceQuery(db, 1, "experience2");
+        TestDB.trackQuery(db, 0, "track1", 0);
+        TestDB.trackQuery(db, 1, "track2", 1);
+
+        IExperience experience = null;
+        for (IExperience ex : dataSource.getExperiences())
+            if (ex.getName().equals("experience"))
+                experience = ex;
+
+        Iterator<ITrack> iterator = experience.getTracks().iterator();
+        ITrack track = iterator.next();
+        assertTrue(!iterator.hasNext());
+        assertEquals("track1", track.name());
+    }
+
+}
